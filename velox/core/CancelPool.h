271ad2d9aa6ad4c3ba19a76f123525d131db115f
oerling <oerling@fb.com>
2021-08-24 05:00:11 -0700

    xx

--- velox/core/CancelPool.h
+++ velox/core/CancelPool.h
@@ -39,58 +39,139 @@ enum class StopReason {
   kAlreadyOnThread
 };
 
+// Represents a Driver's state. This is used for cancellation, forcing release
+// of  and for waiting for memory.
+struct ThreadState {
+  // The thread currently running this.
+  std::thread::id thread;
+  // True if running on a thread. A Driver can be on a thread but in a
+  // waiting state, e.g. for memory, for RPC results.
+  bool isOnThread{false};
+  // True if being terminated or already terminated.
+  bool isTerminated{false};
+  // True if there is a future outstanding that will schedule this on an
+  // executor thread when some promise is realized.
+  bool hasBlockingFuture{false};
+  // True if on thread but in a section waiting for RPC or memory
+  // strategy decision. The thread is not supposed to access its
+  // memory, which a third party can revoke while the thread is in
+  // this state.
+  bool isCancelFree{false};
+};
+
 class CancelPool {
  public:
   // Returns kNone if no pause or terminate is requested. The thread count is
   // incremented if kNone is returned. If something else is returned the
   // calling tread should unwind and return itself to its pool.
-  StopReason enter(bool* isOnThread, bool* isTerminated) {
+  StopReason enter(ThreadState& state) {
     std::lock_guard<std::mutex> l(mutex_);
-    if (*isTerminated) {
+    if (state.isTerminated) {
       return StopReason::kAlreadyTerminated;
     }
-    if (*isOnThread) {
+    if (state.isOnThread) {
       return StopReason::kAlreadyOnThread;
     }
     auto reason = shouldStopLocked();
     if (reason == StopReason::kTerminate) {
-      *isTerminated = true;
+      state.isTerminated = true;
     }
     if (reason == StopReason::kNone) {
       ++numThreads_;
-      *isOnThread = true;
+      state.isOnThread = true;
+      state.thread = std::this_thread::get_id();
+      state.hasBlockingFuture = false;
     }
     return reason;
   }
 
-  StopReason enterForTerminate(bool* isOnThread, bool* isTerminated) {
+  StopReason enterForTerminate(ThreadState& state) {
     std::lock_guard<std::mutex> l(mutex_);
-    if (*isOnThread || *isTerminated) {
-      *isTerminated = true;
+    if (state.isOnThread || state.isTerminated) {
+      state.isTerminated = true;
       return StopReason::kAlreadyOnThread;
     }
-    *isTerminated = true;
-    *isOnThread = true;
+    state.isTerminated = true;
+    state.isOnThread = true;
     return StopReason::kTerminate;
   }
 
-  StopReason leave(bool* isOnThread, bool* isTerminated) {
+  StopReason leave(ThreadState& state) {
     std::lock_guard<std::mutex> l(mutex_);
     if (--numThreads_ == 0) {
-      for (auto& promise : finishPromises_) {
-        promise.setValue(true);
-      }
-      finishPromises_.clear();
+      finished();
     }
-    *isOnThread = false;
-    if (*isTerminated) {
+    state.thread = std::thread::id(); // no thread.
+    state.isOnThread = false;
+    if (state.isTerminated) {
       return StopReason::kTerminate;
     }
     auto reason = shouldStopLocked();
     if (reason == StopReason::kTerminate) {
-      *isTerminated = true;
+      state.isTerminated = true;
     }
     return reason;
+  }
+
+  // Enters a cancel-free section where the caller stays on thread but
+  // is not accounted as being on the thread.  Returns kNone if no
+  // terminate is requested. The thread count is decremented if kNone
+  // is returned. If thread count goes to zero, waiting promises are
+  // realized. If kNone is not returned the calling tread should
+  // unwind and return itself to its pool.
+  StopReason enterCancelFree(ThreadState& state) {
+    VELOX_CHECK(!state.hasBlockingFuture);
+    VELOX_CHECK(state.isOnThread);
+    std::lock_guard<std::mutex> l(mutex_);
+    if (state.isTerminated) {
+      return StopReason::kAlreadyTerminated;
+    }
+    if (!state.isOnThread) {
+      return StopReason::kAlreadyTerminated;
+    }
+    auto reason = shouldStopLocked();
+    if (reason == StopReason::kTerminate) {
+      state.isTerminated = true;
+    }
+    // A pause will not stop entering the cancel free section. It will
+    // just ack that the thread is no longer in inside the
+    // CancelPool. The pause can wait at the exit of the cancel free
+    // section.
+    if (reason == StopReason::kNone || reason == StopReason::kPause) {
+      if (--numThreads_ == 0) {
+        finished();
+      }
+      state.isCancelFree = true;
+    }
+    return StopReason::kNone;
+  }
+
+  StopReason leaveCancelFree(ThreadState& state) {
+    for (;;) {
+      {
+        std::lock_guard<std::mutex> l(mutex_);
+        ++numThreads_;
+        state.isCancelFree = false;
+        if (state.isTerminated) {
+          return StopReason::kAlreadyTerminated;
+        }
+        if (terminateRequested_) {
+          state.isTerminated = true;
+          return StopReason::kTerminate;
+        }
+        if (!pauseRequested_) {
+          // For yield or anything but pause  we return here.
+          return StopReason::kNone;
+        }
+        --numThreads_;
+      }
+      // If the pause flag is on when trying to reenter, sleep a while
+      // outside of the mutex and recheck. This is rare and not time
+      // critical. Can happen if memory interrupt sets pause while
+      // already inside a cancel free section for other reason, like
+      // IO.
+      std::this_thread::sleep_for(std::chrono::milliseconds(10)); // NOLINT
+    }
   }
 
   // Returns a stop reason without synchronization. If the stop reason
@@ -111,6 +192,7 @@ class CancelPool {
   }
 
   void requestPause(bool pause) {
+    std::lock_guard<std::mutex> l(mutex_);
     pauseRequested_ = pause;
   }
 
@@ -125,6 +207,10 @@ class CancelPool {
 
   bool terminateRequested() const {
     return terminateRequested_;
+  }
+
+  bool pauseRequested() const {
+    return pauseRequested_;
   }
 
   // Returns a future that is completed when all threads have acknowledged
@@ -147,6 +233,13 @@ class CancelPool {
   }
 
  private:
+  void finished() {
+    for (auto& promise : finishPromises_) {
+      promise.setValue(true);
+    }
+    finishPromises_.clear();
+  }
+
   StopReason shouldStopLocked() {
     if (terminateRequested_) {
       return StopReason::kTerminate;
