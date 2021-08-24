10b0dff69faa3c67cf2dfd5207a49c4407a1fedc
oerling <oerling@fb.com>
2021-08-24 05:41:40 -0700

    xx

--- velox/exec/tests/PartitionedOutputBufferManagerTest.cpp
+++ velox/exec/tests/PartitionedOutputBufferManagerTest.cpp
@@ -40,7 +40,7 @@ class PartitionedOutputBufferManagerTest : public testing::Test {
       int numDrivers) {
     auto queryCtx = core::QueryCtx::create();
     bufferManager_->removeTask(taskId);
-    auto task = std::make_shared<Task>(taskId, queryCtx);
+    auto task = std::make_shared<Task>(taskId, nullptr, 0, std::move(queryCtx));
     bufferManager_->initializeTask(task, numDestinations, numDrivers);
     return task;
   }
