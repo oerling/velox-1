796b0587f36a67175c1c76b07435bb119ee172ac
oerling <oerling@fb.com>
2021-10-04 08:13:59 -0700

    xx

--- velox/common/base/Bloom.h
+++ velox/common/base/Bloom.h
@@ -61,8 +61,10 @@ class Bloom {
     bloom[index] |= mask;
   }
 
-  inline static bool
-  test(const uint64_t* FOLLY_NONNULL bloom, int32_t bloomSize, uint64_t hashCode) {
+  inline static bool test(
+      const uint64_t* FOLLY_NONNULL bloom,
+      int32_t bloomSize,
+      uint64_t hashCode) {
     auto mask = bloomMask(hashCode);
     auto index = bloomIndex(bloomSize, hashCode);
     return mask == (bloom[index] & mask);
