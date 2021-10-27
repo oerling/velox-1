d0805f4e0b8a4cf9f8031b531bca43ed21ee486d
oerling <oerling@fb.com>
2021-10-27 12:31:11 -0700

    xx

--- velox/common/caching/SsdCache.cpp
+++ velox/common/caching/SsdCache.cpp
@@ -60,13 +60,10 @@ SsdFile::SsdFile(
       maxRegions_(maxRegions),
       filename_(filename) {
   int32_t oDirect = 0;
-  #ifdef linux
+#ifdef linux
   oDirect = FLAGS_ssd_odirect ? O_DIRECT : 0;
 #endif
-  fd_ = open(
-      filename.c_str(),
-      O_CREAT | O_RDWR | oDirect,
-      S_IRUSR | S_IWUSR);
+  fd_ = open(filename.c_str(), O_CREAT | O_RDWR | oDirect, S_IRUSR | S_IWUSR);
   if (fd_ < 0) {
     LOG(ERROR) << "Cannot open or create " << filename << " error " << errno;
     exit(1);
