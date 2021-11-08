56e5061304f66364eaeb465aa80aa62ace3e8d5f
oerling <oerling@fb.com>
2021-11-08 12:01:13 -0800

    xx

--- velox/type/Filter.cpp
+++ velox/type/Filter.cpp
@@ -119,7 +119,7 @@ BigintValuesUsingHashTable::BigintValuesUsingHashTable(
     : Filter(true, nullAllowed, FilterKind::kBigintValuesUsingHashTable),
       min_(min),
       max_(max),
-      values_(values){
+      values_(values) {
   VELOX_CHECK(min < max, "min must be less than max");
   VELOX_CHECK(values.size() > 1, "values must contain at least 2 entries");
 
@@ -179,7 +179,7 @@ bool BigintValuesUsingHashTable::testInt64Range(
     return testInt64(min);
   }
 
-  if(min > max_ || max < min_) {
+  if (min > max_ || max < min_) {
     return false;
   }
   auto it = std::lower_bound(values_.begin(), values_.end(), min);
