477d5872a2a5d178812e1fbf7db2922bfc63e84d
oerling <oerling@fb.com>
2022-01-23 12:52:27 -0800

    xx

--- velox/common/caching/FileGroupStats.h
+++ velox/common/caching/FileGroupStats.h
@@ -111,25 +111,26 @@ class GroupTracker {
 // Set of file group stats. There is one instance per SSD cache.
 class FileGroupStats {
  public:
+  FileGroupStats();
   // Records ScanTracker::recordReference at group level
   void recordReference(
       uint64_t /*fileId*/,
       uint64_t /*groupId*/,
       TrackingId /*trackingId*/,
-      int32_t /*bytes*/) {}
+      int32_t /*bytes*/);
 
   // Records ScanTracker::recordRead at group level
   void recordRead(
       uint64_t /*fileId*/,
       uint64_t /*groupId*/,
       TrackingId /*trackingId*/,
-      int32_t /*bytes*/) {}
+      int32_t /*bytes*/);
 
   // Records the existence of a distinct file inside 'groupId'
   void recordFile(
       uint64_t /*fileId*/,
       uint64_t /*groupId*/,
-      int32_t /*numStripes*/) {}
+      int32_t /*numStripes*/);
 
   // Returns true if groupId, trackingId qualify the data to be cached to SSD.
   bool shouldSaveToSsd(uint64_t groupId, TrackingId trackingId) const;
