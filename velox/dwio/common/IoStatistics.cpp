19c9f3313d3d06c973686ea4d93557ec75202c86
oerling <oerling@fb.com>
2022-01-28 09:25:23 -0800

    xx

--- velox/dwio/common/IoStatistics.cpp
+++ velox/dwio/common/IoStatistics.cpp
@@ -45,7 +45,7 @@ uint64_t IoStatistics::outputBatchSize() const {
 }
 
 uint64_t IoStatistics::incRawBytesRead(int64_t v) {
-  auto r = v +  rawBytesRead_.fetch_add(v, std::memory_order_relaxed);
+  auto r = v + rawBytesRead_.fetch_add(v, std::memory_order_relaxed);
   if ((int64_t)r < 0) {
     LOG(INFO) << "bing";
   }
