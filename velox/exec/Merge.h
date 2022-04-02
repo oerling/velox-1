5dad7c6e0a97b001cd8aca87caa9b49d35b62df1
Masha Basmanova <mbasmanova@fb.com>
2022-04-01 19:35:39 -0700

    Optimize LocalMerge operator (#1318)
    Summary:
    - Remove extra copying into RowContainer.
    - Use TreeOfLosers for merging.
    - Copy rows to output in bulk.
    
    This version is much faster than previous. Merging 7 streams of 80M rows each on
    1 integer key is 27s vs original 70s and on 2 keys is 32s vs original 290s.
    These numbers include optimizations for FlatVector::copyValuesAndNulls
    (https://github.com/facebookincubator/velox/issues/1316) and BaseVector::compare (https://github.com/facebookincubator/velox/issues/1317)
    
    1 flat int key
    <img width="1047" alt="Screen Shot 2022-03-31 at 7 03 18 AM" src="https://user-images.githubusercontent.com/27965151/161041090-1750a8c0-9f82-4bb1-9e96-5bc2d4929691.png">
    
    2 keys: flat and const:
    
    <img width="1044" alt="Screen Shot 2022-03-31 at 7 03 44 AM" src="https://user-images.githubusercontent.com/27965151/161041104-c38a923d-96dc-4f05-bb2f-a42c2f409b62.png">
    
    Pull Request resolved: https://github.com/facebookincubator/velox/pull/1318
    
    Reviewed By: oerling
    
    Differential Revision: D35274106
    
    Pulled By: mbasmanova
    
    fbshipit-source-id: 3f53545be3411347a78609fb9d45ceeb5acea904

--- velox/exec/Merge.h
+++ velox/exec/Merge.h
@@ -15,10 +15,9 @@
  */
 #pragma once
 
-#include <memory>
 #include "velox/exec/Exchange.h"
 #include "velox/exec/MergeSource.h"
-#include "velox/exec/RowContainer.h"
+#include "velox/exec/TreeOfLosers.h"
 
 namespace facebook::velox::exec {
 
@@ -29,7 +28,7 @@ class Merge : public SourceOperator {
  public:
   Merge(
       int32_t operatorId,
-      DriverCtx* ctx,
+      DriverCtx* driverCtx,
       RowTypePtr outputType,
       const std::vector<std::shared_ptr<const core::FieldAccessTypedExpr>>&
           sortingKeys,
@@ -57,90 +56,177 @@ class Merge : public SourceOperator {
   std::vector<std::shared_ptr<MergeSource>> sources_;
 
  private:
-  static const size_t kBatchSizeInBytes{2 * 1024 * 1024};
-  using SourceRow = std::pair<size_t, char*>;
+  struct SourceStream final : public MergeStream {
+    SourceStream(
+        MergeSource* source,
+        const std::vector<std::pair<ChannelIndex, CompareFlags>>& sortingKeys,
+        uint32_t outputBatchSize)
+        : source_{source},
+          sortingKeys_{sortingKeys},
+          outputRows_(outputBatchSize, false),
+          sourceRows_(outputBatchSize) {
+      keyColumns_.reserve(sortingKeys.size());
+    }
 
-  class Comparator {
-   public:
-    Comparator(
-        const RowTypePtr& outputType,
-        const std::vector<std::shared_ptr<const core::FieldAccessTypedExpr>>&
-            sortingKeys,
-        const std::vector<core::SortOrder>& sortingOrders,
-        RowContainer* rowContainer);
+    /// Returns true and appends a future to 'futures' if needs to wait for the
+    /// source to produce data.
+    bool isBlocked(std::vector<ContinueFuture>& futures) {
+      if (needData_) {
+        return fetchMoreData(futures);
+      }
+      return false;
+    }
 
-    // Returns true if lhs > rhs, false otherwise.
-    bool operator()(const SourceRow& lhs, const SourceRow& rhs) {
-      for (auto& key : keyInfo_) {
-        if (auto result = rowContainer_->compare(
-                lhs.second, rhs.second, key.first, key.second)) {
-          return result > 0;
+    bool hasData() const override {
+      return !atEnd_;
+    }
+
+    /// Returns true if current source row is less then current source row in
+    /// 'other'.
+    bool operator<(const MergeStream& other) const override {
+      const auto& otherCursor = static_cast<const SourceStream&>(other);
+      for (auto i = 0; i < sortingKeys_.size(); ++i) {
+        const auto& key = sortingKeys_[i];
+        if (auto result = keyColumns_[i]->compare(
+                otherCursor.keyColumns_[i],
+                currentSourceRow_,
+                otherCursor.currentSourceRow_,
+                key.second)) {
+          return result < 0;
         }
       }
       return false;
     }
 
+    /// Advances to the next row. Returns true and appends a future to 'futures'
+    /// if runs out of rows in the current batch and needs to wait for the
+    /// source to produce the next batch. The return flag has the meaning of
+    /// 'is-blocked'.
+    bool pop(std::vector<ContinueFuture>& futures) {
+      ++currentSourceRow_;
+      if (currentSourceRow_ == data_->size()) {
+        // Make sure all current data has been copied out.
+        VELOX_CHECK(!outputRows_.hasSelections());
+        return fetchMoreData(futures);
+      }
+
+      return false;
+    }
+
+    /// Records the output row number for the current row. Returns true if
+    /// current row is the last row in the current batch, in which case the
+    /// caller must call 'copyToOutput' before calling pop(). The caller must
+    /// call 'setOutputRow' before calling 'pop'. The output rows must
+    /// monotonically increase in between calls to 'copyToOutput'.
+    bool setOutputRow(vector_size_t row) {
+      outputRows_.setValid(row, true);
+      return currentSourceRow_ == data_->size() - 1;
+    }
+
+    /// Called if either current row is the last row in the current batch or the
+    /// caller accumulated enough output rows across all sources to produce an
+    /// output batch.
+    void copyToOutput(RowVectorPtr& output) {
+      outputRows_.updateBounds();
+
+      if (!outputRows_.hasSelections()) {
+        return;
+      }
+
+      vector_size_t sourceRow = firstSourceRow_;
+      outputRows_.applyToSelected(
+          [&](auto row) { sourceRows_[row] = sourceRow++; });
+
+      for (auto i = 0; i < output->type()->size(); ++i) {
+        output->childAt(i)->copy(
+            data_->childAt(i).get(), outputRows_, sourceRows_.data());
+      }
+
+      outputRows_.clearAll();
+
+      if (currentSourceRow_ == data_->size() - 1) {
+        firstSourceRow_ = 0;
+      } else {
+        firstSourceRow_ = currentSourceRow_;
+      }
+    }
+
    private:
-    std::vector<std::pair<ChannelIndex, CompareFlags>> keyInfo_;
-    RowContainer* rowContainer_;
-  };
+    bool fetchMoreData(std::vector<ContinueFuture>& futures) {
+      ContinueFuture future{false};
+      auto reason = source_->next(data_, &future);
+      if (reason != BlockingReason::kNotBlocked) {
+        needData_ = true;
+        futures.emplace_back(std::move(future));
+        return true;
+      }
 
-  /// Appends next row from the specified source to 'candidates_'. If
-  /// corresponding CursorSource has next row, returns that row and advances the
-  /// cursor. Otherwise, fetches next batch of source rows from MergeSource,
-  /// copies them to rowContainer_ and resets the cursor.
-  BlockingReason pushSource(ContinueFuture* future, size_t sourceId);
+      atEnd_ = !data_;
+      needData_ = false;
+      currentSourceRow_ = 0;
 
-  BlockingReason ensureSourcesReady(ContinueFuture* future);
+      if (data_) {
+        for (auto& child : data_->children()) {
+          child = BaseVector::loadedVectorShared(child);
+        }
+        keyColumns_.clear();
+        for (const auto& key : sortingKeys_) {
+          keyColumns_.push_back(data_->childAt(key.first).get());
+        }
+      }
+      return false;
+    }
 
-  /// Returns "max" row from 'candidates_' and removes that row from
-  /// 'candidates_'. Assumes 'candidates_' is not empty.
-  SourceRow nextOutputRow();
+    MergeSource* source_;
 
-  /// A cursor over an ordered batch of source rows copied into 'rowContainer_'.
-  struct SourceCursor {
+    const std::vector<std::pair<ChannelIndex, CompareFlags>>& sortingKeys_;
+
     /// Ordered source rows.
-    std::vector<char*> rows;
-    /// Index of the next row.
-    vector_size_t index{0};
+    RowVectorPtr data_;
+
+    /// Raw pointers to vectors corresponding to sorting key columns in the same
+    /// order as 'sortingKeys_'.
+    std::vector<BaseVector*> keyColumns_;
+
+    /// Index of the current row.
+    vector_size_t currentSourceRow_{0};
+
     /// True if source has been exhausted.
-    bool atEnd{false};
+    bool atEnd_{false};
 
-    /// Returns true if there is a next row.
-    bool hasNext() {
-      return !atEnd && index < rows.size();
-    }
+    /// True if ran out of rows in 'data_' and needs to wait for the future
+    /// returned by 'source_->next()'.
+    bool needData_{true};
 
-    /// Returns next row and advances the cursor.
-    char* nextUnchecked() {
-      return rows[index++];
-    }
+    /// First source row that hasn't been copied out yet.
+    vector_size_t firstSourceRow_{0};
 
-    /// Copies the 'data' into 'rowContainer' and resets the cursor to point to
-    /// the first row.
-    void reset(const RowVectorPtr& data, RowContainer* rowContainer);
+    /// Output row numbers for source rows that haven't been copied out yet.
+    SelectivityVector outputRows_;
+
+    /// Reusable memory.
+    std::vector<vector_size_t> sourceRows_;
   };
+
+  /// Maximum number of rows in the output batch.
+  const uint32_t outputBatchSize_;
+
+  std::vector<std::pair<ChannelIndex, CompareFlags>> sortingKeys_;
 
   /// A list of cursors over batches of ordered source data. One per source.
   /// Aligned with 'sources'.
-  std::vector<SourceCursor> sourceCursors_;
+  std::vector<SourceStream*> streams_;
 
-  /// Ordered list of output rows.
-  std::vector<char*> rows_;
+  std::unique_ptr<TreeOfLosers<SourceStream>> treeOfLosers_;
 
-  /// Row container to store incoming batches of source data.
-  std::unique_ptr<RowContainer> rowContainer_;
+  /// Number of rows accumulated in 'output_' so far.
+  vector_size_t outputSize_{0};
 
-  /// STL-compatible comparator to compare rows by sorting keys.
-  Comparator comparator_;
+  bool finished_{false};
 
-  /// A list of "max" rows from each source. Used to pick the next output row.
-  std::vector<SourceRow> candidates_;
-
-  BlockingReason blockingReason_{BlockingReason::kNotBlocked};
-  ContinueFuture future_;
-  size_t numSourcesAdded_ = 0;
-  size_t currentSourcePos_ = 0;
+  /// A list of blocking futures for sources. These are populates when a given
+  /// source is blocked waiting for the next batch of data.
+  std::vector<ContinueFuture> sourceBlockingFutures_;
 };
 
 // LocalMerge merges its source's output into a single stream of
