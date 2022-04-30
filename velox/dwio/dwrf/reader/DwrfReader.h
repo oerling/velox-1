46915f751605410b3e858e11496f477f3f32ffbd
Orri Erling <oerling@fb.com>
2022-04-30 16:23:07 -0700

    xx

--- velox/dwio/dwrf/reader/DwrfReader.h
+++ velox/dwio/dwrf/reader/DwrfReader.h
@@ -91,6 +91,11 @@ class DwrfRowReader : public DwrfRowReaderShared {
     return it->second;
   }
 
+    // Returns true if stripe prefetch is enabled and an executor is
+  // specified via the BufferedInput factory.
+  bool mayPrefetch() const;
+  
+
  private:
   using StripeReaderSource = AsyncSource<DwrfRowReader>;
 
