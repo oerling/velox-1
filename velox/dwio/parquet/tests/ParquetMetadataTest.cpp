74a43eca2c018e6b7fca7224e790927c514a4498
Orri Erling <oerling@fb.com>
2022-07-03 18:19:39 -0700

    xx

--- velox/dwio/parquet/tests/ParquetMetadataTest.cpp
+++ velox/dwio/parquet/tests/ParquetMetadataTest.cpp
@@ -17,7 +17,7 @@
 //
 // Created by Ying Su on 2/27/22.
 //
-#include "velox/dwio/parquet/reader/NativeParquetReader.h"
+#include "velox/dwio/parquet/reader/ParquetReader.h"
 #include "ParquetReaderTest.h"
 #include "velox/connectors/hive/HiveConnector.h"
 #include "velox/dwio/dwrf/test/utils/FilterGenerator.h"
@@ -30,48 +30,14 @@ std::unique_ptr<common::ScanSpec> makeScanSpec(
     const SubfieldFilters& filters,
     const std::shared_ptr<const RowType>& rowType);
 
-class NativeParquetReaderTest : public ParquetReaderTest {};
+class ParquetMetadataTest : public ParquetReaderTest {};
 
-TEST_F(NativeParquetReaderTest, readBigint) {
-  // sample.parquet holds two columns (a: BIGINT, b: DOUBLE) and
-  // 20 rows (10 rows per group). Group offsets are 153 and 614.
-  // Data is in plain uncompressed format:
-  //   a: [1..20]
-  //   b: [1.0..20.0]
-  const std::string sample(getExampleFilePath("sample.parquet"));
 
-  ReaderOptions readerOptions;
-  NativeParquetReader reader(
-      std::make_unique<FileInputStream>(sample), readerOptions);
-  //
-  EXPECT_EQ(reader.numberOfRows(), 20ULL);
-
-  auto type = reader.typeWithId();
-  EXPECT_EQ(type->size(), 2ULL);
-  auto col0 = type->childAt(0);
-  EXPECT_EQ(col0->type->kind(), TypeKind::BIGINT);
-  auto col1 = type->childAt(1);
-  EXPECT_EQ(col1->type->kind(), TypeKind::DOUBLE);
-  EXPECT_EQ(type->childByName("a"), col0);
-  EXPECT_EQ(type->childByName("b"), col1);
-
-  RowTypePtr rowType = ROW({"a"}, {BIGINT()});
-  auto filterGenerator =
-      std::make_unique<dwio::dwrf::FilterGenerator>(rowType, 1);
-  auto scanSpec = filterGenerator->makeScanSpec(SubfieldFilters{});
-  RowReaderOptions rowReaderOpts = getReaderOpts(rowType);
-  rowReaderOpts.setScanSpec(scanSpec);
-  auto rowReader = reader.createRowReader(rowReaderOpts);
-  auto expected = vectorMaker_->rowVector({rangeVector<int64_t>(20, 1)});
-  assertReadExpected(
-      rowType, *rowReader, expected, readerOptions.getMemoryPool());
-}
-
-TEST_F(NativeParquetReaderTest, readRowMap) {
+TEST_F(ParquetMetadataTest, readRowMap) {
   const std::string sample(getExampleFilePath("tmp_row.parquet"));
 
   ReaderOptions readerOptions;
-  NativeParquetReader reader(
+  ParquetReader reader(
       std::make_unique<FileInputStream>(sample), readerOptions);
 
   EXPECT_EQ(reader.numberOfRows(), 1ULL);
