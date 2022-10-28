6f1a33fd019b2ccd9860b404423afb21390cbb4f
Orri Erling <oerling@fb.com>
2022-10-28 09:26:49 -0700

    init

---
+++ velox/experimental/gpu/CudaExec.h
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <atomic>
+#include <semaphore>
+
+namespace facebook::velox::cuda {
+
+  template <typename T>
+  using vx_atomic<T> = std::atomic<T>
+  
+template <typename T>
+struct Span {
+  T* data;
+  int32_t size;
+};
+
+
+struct Uop {
+  // Marks that this is in the process reading its input and writing its output. Neither may be touched by other Uops.
+  static constexpr int32_t kRunning = -1;
+
+  // Added to 'state' for each input that does not have a value.
+  static constexpr int32_t kInputNeeded = 1;
+
+  // Added to 'state' to mark that an output has not been consumed. 
+  static constexpr int32_t kOutputConsumed = 1000;
+  
+  vx_atomic<int32_t> state;
+
+  // The  uOPS that must have a result for 'this' to be runnable.
+  Span<Uop*> inputs;
+
+  // The Uops that need a result from 'this' to be runnable.
+  span<Uop*> dependents;
+
+  // The Uops whose result is needed only if 'this' becomes runnable
+  Span<Uop*> conditionalInputs;
+};
+
+  struct Decompress : public Uop {
+    // Double buffer for compressed input.
+    Span<char> compressedData[2];
+    Span<char> decompressedData;
+    // Bit 0 decides between first and second compressedData.
+    uint8_t counter{0};
+  };
+  
+  
+  // Commands from GPU to CPU
+  enum class HostOp {
+    kNeedData,
+    kResultReady
+  }
+
+  // Commands from host to GPU
+    enum GpuOp {
+    kDataReady,
+    kResultConsumed
+  };
+
+  struct HostCommand {
+    HostOp op;
+    int64_t taskId;
+    union {
+      struct {
+	uint64_t fileId;
+	uint64_t offset;
+	int32_t size;
+      } input;
+      struct {
+	// 0 when no more will be produced.
+	int32_t numRows;
+      } ready;
+    } _;
+  };
+
+  struct GpuCommand {
+    static constexpr kUopBatch = 16;
+    GpuOp op;
+    Task* task;
+    union {
+      struct {
+	int32_t numUops;
+	Uop* uops[kUopBatch];
+	uint64_t uopData[kUopBatch];
+      } input;
+    } _;
+  };
+  
+  
+    
+  template <  typename T>
+  struct RingBuffer {
+    Span<T> data;
+    vx_atomic<int32_t> writeIndex_;
+    vx_atomic<int32_t> readIndex_;
+    std::counting_semaphore sem;
+  };
+  
+// Represents the state of a Task kernel
+struct Task {
+  // Runs with up to 'numParallel' concurrent  Uops. Returns When all Uops have a no more input state and have completed running.
+  void run(int32_t numParallel);
+  
+  // Runs runnable 'Uops' in turn, returns when all Uops have a no more input state and none is runnable.
+  void runThread();
+  
+  // Unique id
+  int64_t id;
+  std::counting_semaphore<1> runnableSem;
+  Span<Uop*> uops;
+  
+};
+
+  // Represents the state of interaction with one GPU
+  struct GpuState {
+
+      RingBuffer<HostCommand> hostCommands;
+  RingBuffer<GpuCommand> gpuCommands;
+
+  }
+ 
+
+}
+
