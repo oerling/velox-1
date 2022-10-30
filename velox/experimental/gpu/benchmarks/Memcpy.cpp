7151e5624e0cf420d1319acbd0a724d230680678
Orri Erling <oerling@fb.com>
2022-10-30 13:44:42 -0700

    xx

--- velox/experimental/gpu/benchmarks/Memcpy.cpp
+++ velox/experimental/gpu/benchmarks/Memcpy.cpp
@@ -1,13 +1,15 @@
 
 #include <stdio.h>
-#include <semaphore>
+#include <iostream>
+#include "velox/common/base/Semaphore.h"
 #include <thread>
 #include "velox/common/base/AsyncSource.h"
-#include "velox/common/base/SymdUtil.h"
+#include "velox/common/base/SimdUtil.h"
 #include "velox/common/time/Timer.h"
 
 #include <folly/executors/CPUThreadPoolExecutor.h>
-using facebook::velox;
+
+using namespace facebook::velox;
 
 int64_t sum(int64_t* data, int32_t size) {
   int64_t sum = 0;
@@ -20,11 +22,12 @@ int64_t sum(int64_t* data, int32_t size) {
 struct CopyCallable {
   void* source;
   void* destination;
-  int64_t bytes;
-  std::counting_semaphore* sem;
+  int64_t size;
+  Semaphore* sem;
 
   void operator()() {
-    simd::memcpy(destination, source, bytes);
+    simd::memcpy(destination, source, size);
+    sem->release();
   }
 };
 
@@ -34,7 +37,8 @@ int main(int argc, char** argv) {
   int32_t numRepeats = atoi(argv[1]);
   int32_t kb = atoi(argv[2]);
   int32_t numThreads = atoi(argv[3]);
-  int64_t bytes = kb * 1024;
+      auto chunk = std::max<int64_t>(kb * 1024 / numThreads, 64);
+      int64_t bytes = chunk * numThreads;
   auto executor = std::make_unique<folly::CPUThreadPoolExecutor>(numThreads);
   void* other = malloc(bytes);
   void* source = malloc(bytes);
@@ -45,23 +49,23 @@ int main(int argc, char** argv) {
   memset(destination, 1, bytes);
 
   std::vector<std::shared_ptr<AsyncSource<int32_t>>> sources;
-  std::counting_semaphore sem;
+  Semaphore sem(0);
   std::vector<CopyCallable> ops;
   ops.resize(numThreads);
+  int64_t totalSum = 0;
+  uint64_t totalUsec = 0;
   for (auto repeat = 0; repeat < numRepeats; ++repeat) {
-    sum += sum(other, bytes / sizeof(int64_t));
-    auto offset = 0;
+    totalSum += sum(reinterpret_cast<int64_t*>(other), bytes / sizeof(int64_t));
     uint64_t usec = 0;
     {
       MicrosecondTimer timer(&usec);
       for (auto i = 0; i < numThreads; ++i) {
-        int64_t offset1 = chunk * i / 64 * 64;
-        int64_t offset2 = chunk * (i + 1) / 64 * 64 ops[i].source =
-                              reinterpret_cast<char*>(source) + offset1;
+        int64_t offset1 = chunk * i;
+	ops[i].source = reinterpret_cast<char*>(source) + offset1;
         ops[i].destination = reinterpret_cast<char*>(destination) + offset1;
-        ops[i].size = offset2 - offset1;
-        sem.release();
-        executor.add(ops[i]);
+        ops[i].size = chunk;
+	ops[i].sem = &sem;
+        executor->add(ops[i]);
       }
       for (auto i = 0; i < numThreads; ++i) {
         sem.acquire();
@@ -75,8 +79,9 @@ int main(int argc, char** argv) {
                    bytes,
                    numThreads,
                    totalUsec,
-                   bytes * repeats / static_cast<float>(1 << 30) /
+                   bytes * numRepeats / static_cast<float>(1 << 30) /
                        (static_cast<float>(totalUsec + 1) / 1000000.0))
+	    << " count " << totalSum / numRepeats / bytes 
             << std::endl;
   return 0;
 }
