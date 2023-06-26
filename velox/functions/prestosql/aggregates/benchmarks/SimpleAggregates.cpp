6d96dc13db64961fc1fe9182c79a046899b8ba3b
Orri Erling <oerling@fb.com>
2023-06-25 19:45:17 -0700

    xx

--- velox/functions/prestosql/aggregates/benchmarks/SimpleAggregates.cpp
+++ velox/functions/prestosql/aggregates/benchmarks/SimpleAggregates.cpp
@@ -10,9 +10,11 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governng permissions and
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
 #include <fmt/format.h>
 #include <folly/Benchmark.h>
 #include <folly/init/Init.h>
