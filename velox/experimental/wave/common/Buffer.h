1719a2548e4fd13acc53fed90b49fadfea972eed
Orri Erling <oerling@fb.com>
2023-08-06 14:10:34 -0700

    xx

---
+++ velox/experimental/wave/common/Buffer.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once;
+
+#include <boost/intrusive_ptr.hpp>
+
+namespace facebook::velox::wave {
+
+  class GpuArena;
+  
+  /// Each area of device or universal memory in Wave has a unique
+  /// host side control block with reference and pin counts. If
+  /// unpinned, the memory is not currently accessed by a kernel and
+  /// can be moved. When moving, it suffices to update the pointer in
+  /// Buffer. These are managed via WaveBufferPtr. When the reference count goes to 0, the memory is returned to 'arena' and the Buffer is added to the Buffer free list. 
+class Buffer {
+ public:
+
+    void addRef() {
+    referenceCount_.fetch_add(1);
+  }
+
+  int refCount() const {
+    return referenceCount_;
+  }
+
+  void release();
+
+ private:
+
+
+  std::atomic<int32_t> referenceCount_{0};
+  std::atomic<int32_t> pinCount_{0};
+
+  // Pointer to device/universal memory. If 'referenceCount_' is 0, this is the host pointer to the next  free Buffer in 'arena_'.
+  void* ptr_;
+
+  // Byte size of memory held by 'ptt'. Undefined if 'referenceCount_' is 0.
+  int64_t size_{0};
+
+  // The containeing arena.
+  GpuArena* arena_;
+
+  friend class GpuArena;
+};
+
+ using WaveBufferPtr = boost::intrusive_ptr<Buffer>;
+
+static inline void intrusive_ptr_add_ref(Buffer* buffer) {
+  buffer->addRef();
+}
+
+static inline void intrusive_ptr_release(Buffer* buffer) {
+  buffer->release();
+}
+
+}
+
+
---
+++ velox/experimental/wave/common/Exception.cpp
@@ -0,0 +1,13 @@
+
+
+#include "velox/experimental/wave/common/Exception.h"
+
+#include "velox/common/base/Exceptions.h"
+
+namespace facebook::velox::wave {
+  waveError(const std::string& message) {
+    VELOX_FAIL(messsage);
+  }
+
+  
+}
