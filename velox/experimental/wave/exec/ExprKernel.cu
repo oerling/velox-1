05536815bed33240aa101fb895ffe9cb7d1c2731
Orri Erling <oerling@fb.com>
2023-08-14 21:27:09 -0700

    xx

---
+++ velox/experimental/wave/exec/ExprKernel.cu
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/experimental/wave/exec/WaveCore.cuh"
+
+namespace facebook::velox::wave {
+
+
+  template typename<T>
+  func_kPlus(T left, T right) {
+  return left + right;
+}
+
+  
+  template <typename T>
+
+  __device__ filter(FilterInstr* , Control* control, char* shared
+  
+
+  
+__global__ void waveBaseKernel(ThreadBlockProgram** programs) {
+  extern __shared__ __align__(alignof(ScanAlgorithm::TempStorage)) char smem[];
+  auto program = programs[blockIdx.x];
+  int32_t blockBase = program->begin;
+  for (auto i = 0; i < program->numInstructions; ++i) {
+    auto instruction = program->instructions[i];
+    switch (instruction->opCode) {}
+  }
+}
+} // namespace facebook::velox::wave
