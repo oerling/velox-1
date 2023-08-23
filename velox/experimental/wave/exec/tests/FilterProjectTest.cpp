cd5136ffa95cb29fbdf8bea4c19cb1310c865026
Orri Erling <oerling@fb.com>
2023-08-22 18:05:58 -0700

    xx

--- velox/experimental/wave/exec/tests/FilterProjectTest.cpp
+++ velox/experimental/wave/exec/tests/FilterProjectTest.cpp
@@ -81,7 +81,6 @@ TEST_F(FilterProjectTest, roundTrip) {
 }
 
 TEST_F(FilterProjectTest, project) {
-  return;
   std::vector<RowVectorPtr> vectors;
   for (int32_t i = 0; i < 10; ++i) {
     auto vector = std::dynamic_pointer_cast<RowVector>(
