15098ff75ab6874f9909e3a7522ac0fe79d358f7
Orri Erling <oerling@fb.com>
2023-10-23 13:33:18 -0700

    xx

---
+++ velox/dwio/common/CoalescedInputStream.cpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <folly/executors/QueuedImmediateExecutor.h>
+
+#include "velox/common/process/TraceContext.h"
+#include "velox/common/time/Timer.h"
+#include "velox/dwio/common/CoalescedInputStream.h"
+#include "velox/dwio/common/SelectiveBufferedInput.h"
+
+using ::facebook::velox::common::Region;
+
+namespace facebook::velox::dwio::common {
+
+using velox::cache::ScanTracker;
+using velox::cache::TrackingId;
+using velox::memory::MemoryAllocator;
+
+CoalescedInputStream::CoalescedInputStream(
+    SelectiveBufferedInput* bufferedInput,
+    IoStatistics* ioStats,
+    const Region& region,
+    std::shared_ptr<ReadFileInputStream> input,
+    uint64_t fileNum,
+    std::shared_ptr<ScanTracker> tracker,
+    TrackingId trackingId,
+    uint64_t groupId,
+    int32_t loadQuantum)
+    : bufferedInput_(bufferedInput),
+      ioStats_(ioStats),
+      input_(std::move(input)),
+      region_(region),
+      fileNum_(fileNum),
+      tracker_(std::move(tracker)),
+      trackingId_(trackingId),
+      groupId_(groupId),
+      loadQuantum_(loadQuantum) {}
+
+bool CoalescedInputStream::Next(const void** buffer, int32_t* size) {
+  if (position_ >= region_.length) {
+    *size = 0;
+    return false;
+  }
+  loadPosition();
+
+  *buffer = reinterpret_cast<const void**>(run_ + offsetInRun_);
+  *size = runSize_ - offsetInRun_;
+  if (position_ + *size > region_.length) {
+    *size = region_.length - position_;
+  }
+  offsetInRun_ += *size;
+  position_ += *size;
+
+  if (tracker_) {
+    tracker_->recordRead(trackingId_, *size, fileNum_, groupId_);
+  }
+  return true;
+}
+
+void CoalescedInputStream::BackUp(int32_t count) {
+  DWIO_ENSURE_GE(count, 0, "can't backup negative distances");
+
+  uint64_t unsignedCount = static_cast<uint64_t>(count);
+  DWIO_ENSURE(unsignedCount <= offsetInRun_, "Can't backup that much!");
+  position_ -= unsignedCount;
+}
+
+bool CoalescedInputStream::Skip(int32_t count) {
+  if (count < 0) {
+    return false;
+  }
+  uint64_t unsignedCount = static_cast<uint64_t>(count);
+  if (unsignedCount + position_ <= region_.length) {
+    position_ += unsignedCount;
+    return true;
+  }
+  position_ = region_.length;
+  return false;
+}
+
+google::protobuf::int64 CoalescedInputStream::ByteCount() const {
+  return static_cast<google::protobuf::int64>(position_);
+}
+
+void CoalescedInputStream::seekToPosition(PositionProvider& seekPosition) {
+  position_ = seekPosition.next();
+}
+
+std::string CoalescedInputStream::getName() const {
+  return fmt::format(
+      "CoalescedInputStream {} of {}", position_, region_.length);
+}
+
+size_t CoalescedInputStream::positionSize() {
+  // not compressed, so only need 1 position (uncompressed position)
+  return 1;
+}
+
+namespace {
+std::vector<folly::Range<char*>>
+makeRanges(size_t size, memory::Allocation& data, std::string& tinyData) {
+  std::vector<folly::Range<char*>> buffers;
+  if (size > SelectiveBufferedInput::kTinySize) {
+    buffers.reserve(data.numRuns());
+    uint64_t offsetInRuns = 0;
+    for (int i = 0; i < data.numRuns(); ++i) {
+      auto run = data.runAt(i);
+      uint64_t bytes = run.numPages() * memory::AllocationTraits::kPageSize;
+      uint64_t readSize = std::min(bytes, size - offsetInRuns);
+      buffers.push_back(folly::Range<char*>(run.data<char>(), readSize));
+      offsetInRuns += readSize;
+    }
+  } else {
+    buffers.push_back(folly::Range<char*>(tinyData.data(), size));
+  }
+  return buffers;
+}
+} // namespace
+
+void CoalescedInputStream::loadSync() {
+  if (region_.length < SelectiveBufferedInput::kTinySize) {
+    tinyData_.resize(region_.length);
+  } else {
+    auto numPages = memory::AllocationTraits::numPages(loadedRegion_.length);
+    if (numPages > data_.numPages()) {
+      bufferedInput_->pool()->allocateNonContiguous(numPages, data_);
+    }
+  }
+
+  process::TraceContext trace("CoalescedInputStream::loadSync");
+
+  ioStats_->incRawBytesRead(loadedRegion_.length);
+  auto ranges = makeRanges(loadedRegion_.length, data_, tinyData_);
+  uint64_t usec = 0;
+  {
+    MicrosecondTimer timer(&usec);
+    input_->read(ranges, loadedRegion_.offset, LogType::FILE);
+  }
+  ioStats_->read().increment(loadedRegion_.length);
+  ioStats_->queryThreadIoLatency().increment(usec);
+}
+
+
+
+void CoalescedInputStream::loadPosition() {
+  auto offset = region_.offset;
+  if (!isLoaded_) {
+    auto load = bufferedInput_->coalescedLoad(this);
+    if (load) {
+      folly::SemiFuture<bool> waitFuture(false);
+      uint64_t usec = 0;
+      {
+        MicrosecondTimer timer(&usec);
+        if (!load->loadOrFuture(&waitFuture)) {
+          auto& exec = folly::QueuedImmediateExecutor::instance();
+          std::move(waitFuture).via(&exec).wait();
+        }
+        loadedRegion_.offset = region_.offset;
+        loadedRegion_.length =
+            load->getData(region_.offset, data_, tinyData_);
+        isLoaded_ = true;
+      }
+      ioStats_->queryThreadIoLatency().increment(usec);
+    }
+  }
+    // Check if position outside of loaded bounds.
+  if (region_.offset + position_ < loadedRegion_.offset ||
+      region_.offset + position_ >= loadedRegion_.offset + loadedRegion_.length) {
+    loadedRegion_.offset = region_.offset + position_;
+    loadedRegion_.length = position_ + loadQuantum_ <= region_.length
+        ? loadQuantum_
+        : region_.length - position_;
+    loadSync();
+  }
+
+  auto offsetInEntry = position_ - (loadedRegion_.offset - region_.offset);
+    if (data_.numPages() == 0) {
+      run_ = reinterpret_cast<uint8_t*>(tinyData_.data());
+      runSize_ = tinyData_.size();
+      offsetInRun_ = offsetInEntry;
+      offsetOfRun_ = 0;
+    } else {
+      data_.findRun(offsetInEntry, &runIndex_, &offsetInRun_);
+      offsetOfRun_ = offsetInEntry - offsetInRun_;
+      auto run = data_.runAt(runIndex_);
+      run_ = run.data();
+      runSize_ = run.numPages() * memory::AllocationTraits::kPageSize;
+      if (offsetOfRun_ + runSize_ > loadedRegion_.length) {
+        runSize_ = loadedRegion_.length - offsetOfRun_;
+      }
+    }
+}
+
+} // namespace facebook::velox::dwio::common
---
+++ velox/dwio/common/CoalescedInputStream.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "velox/common/caching/FileIds.h"
+#include "velox/common/caching/ScanTracker.h"
+#include "velox/common/io/IoStatistics.h"
+#include "velox/dwio/common/InputStream.h"
+#include "velox/dwio/common/SeekableInputStream.h"
+
+namespace facebook::velox::dwio::common {
+
+class SelectiveBufferedInput;
+
+class CoalescedInputStream : public SeekableInputStream {
+ public:
+  CoalescedInputStream(
+      SelectiveBufferedInput* bufferedInput,
+      IoStatistics* ioStats,
+      const velox::common::Region& region,
+      std::shared_ptr<ReadFileInputStream> input,
+      uint64_t fileNum,
+      std::shared_ptr<cache::ScanTracker> tracker,
+      cache::TrackingId trackingId,
+      uint64_t groupId,
+      int32_t loadQuantum);
+
+  bool Next(const void** data, int* size) override;
+  void BackUp(int count) override;
+  bool Skip(int count) override;
+  google::protobuf::int64 ByteCount() const override;
+  void seekToPosition(PositionProvider& position) override;
+  std::string getName() const override;
+  size_t positionSize() override;
+
+  // Testing function to access loaded state.
+  void testingData(velox::common::Region&loadedRegion, memory::Allocation*& data, std::string*& tinyData) {
+    loadedRegion = loadedRegion_;
+    data = &data_;
+    tinyData = &tinyData_;
+  }
+  
+ private:
+  // Ensures that the current position is covered by 'data_'.
+  void loadPosition();
+
+  // Synchronously sets 'data_' to cover loadedRegion_'.
+  void loadSync();
+
+  SelectiveBufferedInput* const bufferedInput_;
+  IoStatistics* ioStats_;
+  std::shared_ptr<ReadFileInputStream> input_;
+  // The region of 'input' 'this' ranges over.
+  const velox::common::Region region_;
+  const uint64_t fileNum_;
+  std::shared_ptr<cache::ScanTracker> tracker_;
+  const cache::TrackingId trackingId_;
+  const uint64_t groupId_;
+
+
+  velox::common::Region loadedRegion_;
+
+  // Maximum number of bytes read from 'input' at a time.
+  const int32_t loadQuantum_;
+
+  // Allocation with loaded data. Has space for region.length or loadQuantum_ bytes, whichevr is less.
+  memory::Allocation data_;
+
+  // Contains the data if the range is too small for Allocation.
+  std::string tinyData_;
+  
+  // Offset of current run from start of 'data_'
+  uint64_t offsetOfRun_;
+
+  // Pointer  to start of  current run in 'entry->data()' or
+  // 'entry->tinyData()'.
+  uint8_t* run_{nullptr};
+  // Position of stream relative to 'run_'.
+  int offsetInRun_{0};
+  // Index of run in 'data_'
+  int runIndex_ = -1;
+  // Number of valid bytes above 'run_'.
+  uint32_t runSize_ = 0;
+  // Position relative to 'region_.offset'.
+  uint64_t position_ = 0;
+
+  // 
+  bool isLoaded_{false};
+};
+
+} // namespace facebook::velox::dwio::common
