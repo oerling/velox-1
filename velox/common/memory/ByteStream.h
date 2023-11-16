394086ec2d7b441ae5bc9f123d71e98081b7ad7f
Orri Erling <oerling@fb.com>
2023-11-16 11:12:58 -0800

    xx

--- velox/common/memory/ByteStream.h
+++ velox/common/memory/ByteStream.h
@@ -294,45 +294,6 @@ class ByteStream {
     return lastRangeEnd_;
   }
 
-  /// Sets 'current_' to point to the next range of input.  // The
-  /// input is consecutive ByteRanges in 'ranges_' for the base class
-  /// but any view over external buffers can be made by specialization.
-  virtual void next(bool throwIfPastEnd = true);
-
-  uint8_t readByte();
-
-  void readBytes(uint8_t* bytes, int32_t size);
-
-  template <typename T>
-  T read() {
-    if (current_->position + sizeof(T) <= current_->size) {
-      current_->position += sizeof(T);
-      return *reinterpret_cast<const T*>(
-          current_->buffer + current_->position - sizeof(T));
-    }
-    // The number straddles two buffers. We read byte by byte and make
-    // a little-endian uint64_t. The bytes can be cast to any integer
-    // or floating point type since the wire format has the machine byte order.
-    static_assert(sizeof(T) <= sizeof(uint64_t));
-    uint64_t value = 0;
-    for (int32_t i = 0; i < sizeof(T); ++i) {
-      value |= static_cast<uint64_t>(readByte()) << (i * 8);
-    }
-    return *reinterpret_cast<const T*>(&value);
-  }
-
-  template <typename Char>
-  void readBytes(Char* data, int32_t size) {
-    readBytes(reinterpret_cast<uint8_t*>(data), size);
-  }
-
-  /// Returns a view over the read buffer for up to 'size' next
-  /// bytes. The size of the value may be less if the current byte
-  /// range ends within 'size' bytes from the current position.  The
-  /// size will be 0 if at end.
-  std::string_view nextView(int32_t size);
-
-  void skip(int32_t size);
 
   /// Returns a range of 'size' items of T. If there is no contiguous space in
   /// 'this', uses 'scratch' to make a temp block that is appended to 'this' in
