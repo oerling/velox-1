7d430921eca88a9f0dff19ac201ebda938aa06e7
Orri Erling <oerling@fb.com>
2023-12-07 08:51:42 -0800

    xx

--- velox/exec/tests/HashTableTest.cpp
+++ velox/exec/tests/HashTableTest.cpp
@@ -528,7 +528,7 @@ TEST_P(HashTableTest, int2SparseArray) {
 TEST_P(HashTableTest, int2SparseNormalized) {
   auto type = ROW({"k1"}, {BIGINT()});
   keySpacing_ = 1;
-  testCycle(BaseHashTable::HashMode::kNormalizedKey, (224 * (1<< 20) - 1) / 255, 255, type, 1);
+  testCycle(BaseHashTable::HashMode::kNormalizedKey, (448 * (1<< 20) - 1) / 255, 255, type, 1);
 }
 
 TEST_P(HashTableTest, int2SparseNormalizedMostMiss) {
