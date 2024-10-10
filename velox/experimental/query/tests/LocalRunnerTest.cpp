57d26202c2ba42945007eec9ccb8e76c9bf00ef3
Ubuntu <ubuntu@ip-172-31-39-11.us-east-2.compute.internal>
2024-10-10 18:37:15 +0000

    xx

---
+++ velox/experimental/query/tests/LocalRunnerTest.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+using namespace facebook::velox::exec;
+
+
+class LocalRunnerTest : public testing::Test {
+ protected:
+  void SetUp() override {
+    memory::MemoryManager::testingSetInstance({});
+    rootPool_ = memory::memoryManager()->addRootPool("velox_sql");
+    pool_ = rootPool_->addLeafChild("optimizer");
+    allocator_ = std::make_unique<HashStringAllocator>(pool_.get());
+    context_ = std::make_unique<QueryGraphContext>(*allocator_);
+    queryCtx() = context_.get();
+    functions::prestosql::registerAllScalarFunctions();
+    aggregate::prestosql::registerAllAggregateFunctions();
+    parse::registerTypeResolver();
+    filesystems::registerLocalFileSystem();
+    if (!registered) {
+      registered = true;
+      parquet::registerParquetReaderFactory();
+    }
+
+  }
+
+};
+
+TEST_F(LocalRunnerTest, basic) {
+
+
+}
