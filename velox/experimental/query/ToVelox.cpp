953a23552fea67d84c23285945ecf7203e5713d8
Orri Erling <oerling@fb.com>
2024-12-08 10:31:25 -0800

    xx

--- velox/experimental/query/ToVelox.cpp
+++ velox/experimental/query/ToVelox.cpp
@@ -72,15 +72,20 @@ void filterUpdated(BaseTableCP table) {
     columns.push_back(connector->createColumnHandle(*layoutData, column->name());
   }
   auto handle = connector->createTableHandle(
-					     *layoutData,
-					     layout->name(),
-					     columns,
-					     optimization.evaluator(),
+      *layoutData,
+      layout->name(),
+      columns,
+      optimization.evaluator(),
       std::move(allFilters),
-					     rejectedFilters);
-					     );
-  optimization->setLeafHandle(table->id(), handle, std::move(rejectedFilters));
-y  optimization->setLeafSelectivity(*const_cast<BaseTable*>(table));
+      rejectedFilters);
+                                             );
+                                             optimization->setLeafHandle(
+                                                 table->id(),
+                                                 handle,
+                                                 std::move(rejectedFilters));
+                                             y optimization->setLeafSelectivity(
+                                                 *const_cast<BaseTable*>(
+                                                     table));
 }
 
 core::PlanNodeId Optimization::nextId(const RelationOp& op) {
@@ -135,8 +140,7 @@ MultiFragmentPlanPtr Optimization::toVeloxPlan(
       std::move(stages), options);
 }
 
-RowTypePtr Optimization::makeOutputType(
-    const ColumnVector& columns) {
+RowTypePtr Optimization::makeOutputType(const ColumnVector& columns) {
   std::vector<std::string> names;
   std::vector<TypePtr> types;
   for (auto i = 0; i < columns.size(); ++i) {
@@ -152,7 +156,6 @@ RowTypePtr Optimization::makeOutputType(
         auto* runnerColumn =
             runnerTable->findColumn(std::string(column->name()));
         VELOX_CHECK_NOT_NULL(runnerColumn);
-
       }
     }
     auto name = makeVeloxExprWithNoAlias_ ? std::string(column->name())
