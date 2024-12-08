fba522f63870c1dc7bed24b408b43d9a62e3adae
Orri Erling <oerling@fb.com>
2024-12-08 10:30:56 -0800

    xx

--- velox/experimental/query/ToVelox.cpp
+++ velox/experimental/query/ToVelox.cpp
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#include "velox/connectors/hive/HiveConnector.h"
-#include "velox/connectors/hive/TableHandle.h"
 #include "velox/core/PlanNode.h"
 #include "velox/exec/HashPartitionFunction.h"
 #include "velox/exec/tests/utils/PlanBuilder.h"
@@ -74,20 +72,15 @@ void filterUpdated(BaseTableCP table) {
     columns.push_back(connector->createColumnHandle(*layoutData, column->name());
   }
   auto handle = connector->createTableHandle(
-      *layoutData,
-      layout->name(),
-      columns,
-      optimization.evaluator(),
+					     *layoutData,
+					     layout->name(),
+					     columns,
+					     optimization.evaluator(),
       std::move(allFilters),
-      rejectedFilters);
-                                             );
-                                             optimization->setLeafHandle(
-                                                 table->id(),
-                                                 handle,
-                                                 std::move(rejectedFilters));
-                                             optimization->setLeafSelectivity(
-                                                 *const_cast<BaseTable*>(
-                                                     table));
+					     rejectedFilters);
+					     );
+  optimization->setLeafHandle(table->id(), handle, std::move(rejectedFilters));
+y  optimization->setLeafSelectivity(*const_cast<BaseTable*>(table));
 }
 
 core::PlanNodeId Optimization::nextId(const RelationOp& op) {
@@ -142,7 +135,8 @@ MultiFragmentPlanPtr Optimization::toVeloxPlan(
       std::move(stages), options);
 }
 
-RowTypePtr Optimization::makeOutputType(const ColumnVector& columns) {
+RowTypePtr Optimization::makeOutputType(
+    const ColumnVector& columns) {
   std::vector<std::string> names;
   std::vector<TypePtr> types;
   for (auto i = 0; i < columns.size(); ++i) {
@@ -158,6 +152,7 @@ RowTypePtr Optimization::makeOutputType(const ColumnVector& columns) {
         auto* runnerColumn =
             runnerTable->findColumn(std::string(column->name()));
         VELOX_CHECK_NOT_NULL(runnerColumn);
+
       }
     }
     auto name = makeVeloxExprWithNoAlias_ ? std::string(column->name())
