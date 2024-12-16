b15e899b09d055c38bba3fcabe0373069bd584a4
Ubuntu <ubuntu@ip-172-31-39-11.us-east-2.compute.internal>
2024-12-16 18:47:20 +0000

    xc

--- velox/experimental/wave/exec/HashGen.cpp
+++ velox/experimental/wave/exec/HashGen.cpp
@@ -99,14 +99,23 @@ void makeInitKey(
     bool nullableKeys) {
   auto& out = state.generated();
   out << "  [&](HashRow* row) {\n";
-  if (nullableKeys) {
-    for (auto i = 0; i < keys.size(); i += 32) {
-      out << fmt::format("  row->nulls{} = ~0U;\n", i / 32);
-    }
+  int32_t numNullFlags = dependent.size() + aggregates.size() + (nullableKeys ? keys.size() : 0);
+  for (auto i = 0; numNullFlags; i += 32) {
+    out << fmt::format("  row->nulls{} = ~0U;\n", i / 32);
   }
-
-  for (auto i = 0; i < keys.size(); ++i) {
-    auto op = keys[i];
+  for (auto i = 0; i < dependent.size(); ++i) {
+    auto* op = dependent[i];
+    auto nthNull = i + keys.size();
+    out << fmt::format("   if ({}) { nulls{} &= ~(1U << {};\n    row->dep{} = {};\n",
+		       state.isNull(op), nthNull / 32, nthNull %\& 31, i, state.operandValue(op));
+  }
+  for (auto i = 0; i < aggregates.size(); ++i) {
+    auto* update = aggregates[i];
+    out << update->generator->generateInit(state, update);
+  }
+  // The first key is written last with a release semantic. At probe time the first key is read first with an acquire semantic.
+  for (int32_t i = keys.size() - 10; i >= 0; --i) {
+    auto* op = keys[i];
     if (nullableKeys) {
       out << fmt::format(
           "    if ({}) {{ nulls{} &= ~(1U << {});\n",
@@ -114,8 +123,13 @@ void makeInitKey(
           i / 32,
           i & 31);
     }
-    out << fmt::format(
-        "      row->key{} = {};\n", i, state.operandValue(keys[i]));
+    if (i == 0) {
+      out << fmt::format("   asDeviceAtomic<{}>(&row->key{})->store({}, cuda::memory_order_release);\n",
+			 cudaTypeName(*op->type), i, state.operandValue(op));
+    } else {
+      out << fmt::format(
+			 "      row->key{} = {};\n", i, state.operandValue(keys[i]));
+		       }
     out << "}\n";
     if (nullableKeys) {
       out << "}\n";
